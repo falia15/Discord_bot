78b69baf1349881039fec11665e546f7f82b35fc
Falia <math.1510@hotmail.fr>
2018-11-03 15:20:45 +0100

    Fix last bug and add comment before first prod test

--- src/Class/Server.js
+++ src/Class/Server.js
@@ -1,18 +1,19 @@
-
+/**
+ * Class used to handle multi discord server
+ */
 class Server {
 
+    /**
+     * Initialise array of server
+     */
     constructor(){
         this.servers = [];
     }
 
-    addHangman(serverId, hangman){
-        this.servers[serverId] = hangman;
-    }
-
-    getHangman(serverId){
-        return this.servers[serverId];
-    }
-
+    /**
+     * Get the server id of a discord server
+     * @param {*string} message 
+     */
     getIdServer(message){
         if((message.guild !== null) && message.guild.id !== null){
             return message.guild.id;
@@ -20,6 +21,24 @@ class Server {
         }
         return null;
     }
+
+    /**
+     * Insinde the servers array, Store an hangman object define by the serverId
+     * @param {*string} serverId 
+     * @param {*object} hangman 
+     */
+    addHangman(serverId, hangman){
+        this.servers[serverId] = hangman;
+    }
+
+    /**
+     * Get hangman object by server id
+     * @param {*} serverId 
+     */
+    getHangman(serverId){
+        return this.servers[serverId];
+    }
+
 
 }
 
